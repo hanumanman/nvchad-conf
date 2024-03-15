7f86382e0764724afbe608c132131a29c145b98b
valencia-nguyen <valencia.nguyen@sotatek.com>
2024-03-15 18:10:34 +0700

    set default relative number & highlight on yank

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,11 @@
-require "nvchad.options"
+require("nvchad.options")
 
 -- add yours here!
+
+vim.wo.relativenumber = true
+vim.cmd([[
+augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=50})
+augroup END
+]])
