f766683f9a31c4bee7df494a92cb70f78717cbeb
valencia-nguyen <valencia.nguyen@sotatek.com>
2024-03-17 23:23:26 +0700

    add mini.cursorword & ts-context-commentstring

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -36,6 +36,7 @@ return {
 		},
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
+			"JoosepAlviste/nvim-ts-context-commentstring",
 			"windwp/nvim-ts-autotag",
 		},
 	},
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -17,9 +17,21 @@ return {
   keys = {
     { "z", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "Z", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 	},
+
+	-- Config for commenting tsx file
+	{
+		"numToStr/Comment.nvim",
+		enable_autocmd = false,
+		opts = {
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		},
+	},
+
+	-- Underline current word under cursor
+	{ "echasnovski/mini.cursorword", version = false, opts = {}, lazy = false },
 }
